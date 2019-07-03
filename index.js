7467d2311bd4ca36a93e783e35e5e2129bbcd9a8
lillian rose winter <34801278+lilyshibe@users.noreply.github.com>
2019-07-02 21:08:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,30 @@ client.on('message', (message) => {
         return message.content.startsWith(text);
     }
  
+ 
+
+const clean = text => {
+  if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+      return text;
+}
+
+if(msw("sang-eval")) {
+ if (message.author.id !== "125727575422009344") return;
+ try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+ 
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+ 
+      message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+}
+ 
     if (msw("sangwoo is gay")) {
         message.channel.send({
             files: ['./imnotgay.png']
